d731f78cff5304ca06caad02b29f40f357b88292
williewilkerson27 <williamwilkerson27@outlook.com>
2024-01-19 18:59:34 -0500

    The form is showing up after New Book is clicked. Basic styling is done but is subject to change

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ function book() {
 }
 
 function addBookToLibrary() {
-    myLibrary.map(element)
+    
 }
+
+// we want the user to see the form after new book has been clicked
+let newBookBtn = document.querySelector("#new-book-btn");
+newBookBtn.addEventListener('click', function() {
+    let newBookForm = document.querySelector('#new-book-form');
+    console.log(newBookForm)
+    newBookForm.style.display = 'block';
+})

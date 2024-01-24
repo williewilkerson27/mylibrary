180124cf427c4b15842438527ccafd3eb65312b4
williewilkerson27 <williamwilkerson27@outlook.com>
2024-01-23 20:12:07 -0500

    Rendering in the books the user inputs is mostly complete, just need to style.

--- main.js
+++ main.js
@@ -15,7 +15,22 @@ function addBookToLibrary() {
     let read = document.getElementById('read').checked;
     let newBook = new Book(title, author, page, read);
     myLibrary.push(newBook)
-    console.log(myLibrary)
+    render();
+}
+
+// the purpose of render function is to display the array into actual html, instead of just the console.
+// the purpose of the for-loop is so it iterates through each book that exists
+// libraryEl.innerHTML = "" makes it where it doesnt duplicate the books as it renders
+function render() {
+    let libraryEl = document.querySelector("#library");
+    libraryEl.innerHTML = "";
+    for ( let i=0; i < myLibrary.length; i++ ) {
+        let book = myLibrary[i];
+        let bookEl = document.createElement('div');
+        bookEl.innerHTML = `<p>${book.title}</p>`;
+// appendChild pushes the book to the div   
+        libraryEl.appendChild(bookEl)
+    }
 }
 
 // we want the user to see the form after new book has been clicked
@@ -30,6 +45,5 @@ newBookBtn.addEventListener('click', function() {
 // Since submit tries to go straight to the back end(we dont have yet), we need to preventDefault() so it doesnt do that.
 document.querySelector('#new-book-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Hello Im working')
     addBookToLibrary();
 } )
